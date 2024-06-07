9b8d1987069d7e5a0ca4154e16956a9252c2455e
JayvonK <jaykhuth@gmail.com>
2024-06-06 21:23:00 -0700

    adding datacontext and service

---
+++ Services/Context/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSBackend.Services.Context
+{
+    public class DataContext
+    {
+        
+    }
+}
